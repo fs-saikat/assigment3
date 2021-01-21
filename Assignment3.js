2b68aad466ac97624019b7735cb6cb2d3adccd7e
unknown <fs-saikat>
2021-01-22 04:00:01 +0600

    adding

---
+++ Assignment3.js
@@ -0,0 +1,61 @@
+/*function kilometerToMeter (kilometer){
+     var distance = kilometer/ 1000;
+     return distance;
+}
+var overallDistance = kilometerToMeter(20000);
+*/
+
+
+
+
+
+/*
+
+ function budgetCalculator(phone){
+      var quantity = phone *7;
+      return quantity;
+
+ }
+ 
+  var phonePrice = budgetCalculator(100);
+  
+
+
+
+  function budgetCalculator(laptop){
+     var quantity = laptop *3;
+     return quantity;
+
+}
+
+var laptopPrice = budgetCalculator(500);
+
+
+
+function budgetCalculator(watch){
+     var quantity = watch *4;
+     return quantity;
+}
+ var watchPrice = budgetCalculator(50);
+ 
+
+ var total = phonePrice + watchPrice + laptopPrice;
+*/
+
+
+ 
+ 
+
+
+
+
+ 
+
+ 
+
+
+
+
+
+
+
